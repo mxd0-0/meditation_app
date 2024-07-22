887f11cec659f84e69f7a09b95a1d5208a1ce723
Mohammed <mo7amxd.7@gmail.com>
2024-07-22 23:16:25 +0100

    topappbar

--- app/src/main/java/com/example/meditationapp/MainActivity.kt
+++ app/src/main/java/com/example/meditationapp/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.meditationapp.ui.theme.MeditationAppTheme
 
@@ -18,30 +22,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MeditationAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Upbarr(modifier: Modifier = Modifier) {
+    TopAppBar(title = { Column {
+        Text(text = "Happy Meditation, Moh", fontWeight = FontWeight.SemiBold)
+    } })
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MeditationAppTheme {
-        Greeting("Android")
-    }
+
 }
